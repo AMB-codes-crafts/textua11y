bc88db8f56dd36003ee8317c2658cddbe4aa4b3b
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-06-13 02:56:20 -0400

    semi colon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import ColorPicker from './components/color-picker'
+import ColorPicker from './components/color-picker';
 import { MdCheck, MdClose} from 'react-icons/md';
 
 require('typeface-lora');
