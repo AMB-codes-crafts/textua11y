4c5fc8a2dbda5fe772e5a365d11f11294cd53b89
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-16 13:34:42 -0400

    Setting color of redo icon based on selected color

--- components/ColorPicker/ColorPicker.jsx
+++ components/ColorPicker/ColorPicker.jsx
@@ -3,6 +3,41 @@ import { Icon } from '../../components';
 import colors from './colors.json';
 import styles from './ColorPicker.module.scss';
 
+const getIconColor = (hex_color) => {
+  var red_hex = hex_color.substring(1, 3);
+  var green_hex = hex_color.substring(3, 5);
+  var blue_hex = hex_color.substring(5, 7);
+
+  var red = parseInt(red_hex, 16) / 255.0;
+  if (red <= 0.03928) {
+    red = red / 12.92;
+  } else {
+    red = Math.pow((red + 0.055) / 1.055, 2.4);
+  }
+
+  var green = parseInt(green_hex, 16) / 255.0;
+  if (green <= 0.03928) {
+    green = green / 12.92;
+  } else {
+    green = Math.pow((green + 0.055) / 1.055, 2.4);
+  }
+
+  var blue = parseInt(blue_hex, 16) / 255.0;
+  if (blue <= 0.03928) {
+    blue = blue / 12.92;
+  } else {
+    blue = Math.pow((blue + 0.055) / 1.055, 2.4);
+  }
+
+  var luminance = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+
+  if (luminance > 0.179) {
+    return '#000000';
+  } else {
+    return '#ffffff';
+  }
+};
+
 const ColorPicker = ({ title, onChangeCallback }) => {
   const [selectedColor, setSelectedColor] = useState();
   const [selectedTone, setSelectedTone] = useState();
@@ -71,7 +106,9 @@ const ColorPicker = ({ title, onChangeCallback }) => {
               }
             }}
           >
-            {selectedShade && <Icon name="redo-alt" />}
+            {selectedShade && (
+              <Icon name="redo-alt" color={getIconColor(backgroundColor)} />
+            )}
           </button>
         ))}
       </div>
