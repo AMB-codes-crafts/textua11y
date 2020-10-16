c6e8075b3a75de1571cdf866c60141198eb3f5e3
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-16 12:52:16 -0400

    Adding ability to cycle through color, tone, and shade

--- components/ColorPicker/ColorPicker.jsx
+++ components/ColorPicker/ColorPicker.jsx
@@ -3,7 +3,41 @@ import colors from './colors.json';
 import styles from './ColorPicker.module.scss';
 
 const ColorPicker = ({ title }) => {
-  const [subtitle, setSubtitle] = useState('Pick your color');
+  const [selectedColor, setSelectedColor] = useState();
+  const [selectedTone, setSelectedTone] = useState();
+  const [selectedShade, setSelectedShade] = useState();
+
+  let subtitle;
+  let buttonsToRender;
+  if (!selectedColor) {
+    subtitle = 'Pick your color';
+    buttonsToRender = colors.map(({ name, tones }) => {
+      const at500Tone = tones.find(({ tone }) => tone == 500);
+      return { backgroundColor: at500Tone.default, name };
+    });
+  } else if (!selectedTone) {
+    subtitle = 'Pick your tone';
+    const color = colors.find((color) => color.name === selectedColor);
+    buttonsToRender = color.tones.map((tone) => ({
+      backgroundColor: tone.default,
+      tone: tone.tone,
+    }));
+  } else if (!selectedShade) {
+    subtitle = 'Pick your shade';
+    const color = colors.find((color) => color.name === selectedColor);
+    const tone = color.tones.find(({ tone }) => tone === selectedTone);
+    buttonsToRender = [
+      { shade: 'default', backgroundColor: tone.default },
+      { shade: 'light', backgroundColor: tone.light },
+      { shade: 'dark', backgroundColor: tone.dark },
+    ];
+  } else {
+    const color = colors.find((color) => color.name === selectedColor);
+    const tone = color.tones.find(({ tone }) => tone === selectedTone);
+    const shade = tone[selectedShade];
+    subtitle = `${selectedColor} ${selectedTone} // ${selectedShade}`;
+    buttonsToRender = [{ backgroundColor: shade }];
+  }
 
   return (
     <div className={styles.ColorPicker}>
@@ -12,22 +46,29 @@ const ColorPicker = ({ title }) => {
       <div
         style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
       >
-        {colors.map(({ name, tones }) => {
-          const at500Tone = tones.find(({ tone }) => tone == 500);
-          if (!at500Tone) {
-            return;
-          }
-
-          return (
-            <div
-              style={{
-                width: 50,
-                height: 50,
-                backgroundColor: at500Tone.default,
-              }}
-            ></div>
-          );
-        })}
+        {buttonsToRender.map(({ backgroundColor, name, tone, shade }) => (
+          <button
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: backgroundColor,
+              border: 'none',
+            }}
+            onClick={() => {
+              if (!selectedColor) {
+                setSelectedColor(name);
+              } else if (!selectedTone) {
+                setSelectedTone(tone);
+              } else if (!selectedShade) {
+                setSelectedShade(shade);
+              } else {
+                setSelectedColor();
+                setSelectedTone();
+                setSelectedShade();
+              }
+            }}
+          ></button>
+        ))}
       </div>
     </div>
   );
